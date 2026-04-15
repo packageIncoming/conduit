b714d99bf7f9c097557e97db6c76301dddcbb06e
Mert <mertisik329@gmail.com>
2026-04-15 00:15:53 -0700

    worked on trd00 up to r3

--- conduit.c
+++ conduit.c
@@ -1,5 +1,64 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(){
+int main(int argc, char *argv[]){
+    if (argc <2){
+        fprintf(stderr,"Usage: ./conduit <port number>\n");
+        exit(EXIT_FAILURE);
+    }
     printf("Hello world\n");
+    int port = atoi(argv[1]);
+    printf("Using port %i\n",port);
+
+    // Part 0: making and priming the socket
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);                         // We are creating a TCP socket on IPv4 
+    if (socketFD < 0){
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    int optval=1;
+    if (setsockopt(socketFD,SOL_SOCKET,SO_REUSEADDR,&optval,sizeof optval) <0){ // We are setting SO_REUSEADDR to 1 (TRUE) 
+        perror("setsockopt");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    // now we setup the sockaddr_in struct and call bind using it
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));        // zero the struct, so clear out any garbage data
+    addr.sin_family = AF_INET;             // IPv4 (X.X.X.X) format
+    addr.sin_addr.s_addr = INADDR_ANY;     // all interfaces
+    addr.sin_port = htons(port);           // port, byte-swapped host to network 
+
+
+    if (bind(socketFD,(struct sockaddr *)&addr, sizeof(addr)) == -1){   //bind here
+        perror("bind");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    // now listen on that socket and forever accept connections
+    if(listen(socketFD,128) == -1){  // listen on socketFD with 128 conn backlog
+        perror("listen");
+        close(socketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1){
+        struct sockaddr_in clientAddr;
+        memset(&clientAddr, 0, sizeof(clientAddr));        // zero the struct, so clear out any garbage data
+        
+        int clientFD = accept(socketFD,NULL,NULL);
+        if (clientFD == -1){
+            perror("accept");
+            close(socketFD);
+            continue;
+        }
+    }
+
+
 }
